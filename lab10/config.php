57ea09337d709540e4d7b58fe2a29c5e3926fa3f
jiping-zhang <19302010022@fudan.edu.cn>
2020-06-01 21:05:39 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'root');
+define('DBPASS', '123456');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
